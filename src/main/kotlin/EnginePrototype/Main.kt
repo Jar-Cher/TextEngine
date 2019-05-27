e58cd5b7f691dd01a83b36f85433ac8007e0863d
Jar-Cher <jaroslav.chernyshev@gmail.com>
2019-05-27 12:21:30 +0300

    Initial quest structure tests completed.

--- src/main/kotlin/EnginePrototype/Main.kt
+++ src/main/kotlin/EnginePrototype/Main.kt
@@ -1,37 +1,44 @@
 package EnginePrototype
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonParser
-import com.google.gson.reflect.TypeToken
+import com.google.gson.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import java.io.File
-import com.google.gson.JsonElement
+import tornadofx.*
+import groovy.util.*
+//import javafx.
 
 
+data class Quest(val save: GameState, val starting: GameState, val finishingId: Int, val slides: Array<Slide>)
 
-data class TestModel(
-    val id: Int,
-    val description: String
-)
+data class GameState(val slideId: Int)
 
-data class Person(val name: String, val age: Int, val messages: Map<String, Int>) {
-}
+data class Slide(val slideId: Int, val text: String, val options: Array<Option>)
 
-data class Message(val time: Int, val text: String){
-}
+data class Option(val optionId: Int, val optionText: String, val moveToSlide: Int)
+
+//class MyApp: App(MyView::class)
+
+/*class MyView: View() {
+    override val root = vbox {
+        button("Press me")
+        label("Waiting")
+    }
+}*/
 
 fun main(args: Array<String>) {
 
+    println(Eval.x(4, "2*x"))
+
+
     val gson = Gson()
-    val jsonString = gson.toJson(TestModel(1,"Test"))
-    assertEquals(jsonString, """{"id":1,"description":"Test"}""")
+    //val jsonString = gson.toJson(TestModel(1,"Test"))
+    //assertEquals(jsonString, """{"id":1,"description":"Test"}""")
 
     //val json = """{"name": "Kolineer", "age": "26", "messages" : ["Master Kotlin","At Kolination"]}"""
     //val person1 : Person = gson.fromJson(json, Person::class.java)
 
-    //val json2 = File("input").readText()
-    //val person2 : Person = gson.fromJson(json2, Person::class.java)
+    val json2 = File("input").readText()
+    val testingQuest : Quest = gson.fromJson(json2, Quest::class.java)
 
     val testText = """{
         "data" :
@@ -42,7 +49,7 @@ fun main(args: Array<String>) {
     }"""
 
     val some = JsonParser().parse(testText)
-    val nope = some.asJsonObject.get("data").asJsonObject.entrySet()
-    println(nope)
+    val nope = some.asJsonObject.get("data")
+    println(testingQuest)
 }
 
